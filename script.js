db9d1f84e7867990120e01533828f39c06588ffb
mdrayhan03 <153529870+mdrayhan03@users.noreply.github.com>
2024-08-29 01:55:44 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function animation() {
+    document.getElementsByClassName('first-part')[0].style.transform = "translateX(0)" ;
+    document.getElementsByClassName('img')[0].style.opacity = 1 ;
+    document.getElementsByClassName('from-part')[0].style.transform = "translateX(0)" ;
+    document.getElementsByClassName('to-part')[0].style.transform = "translateX(0)" ;
+}
+animation();
